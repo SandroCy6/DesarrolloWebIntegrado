9dae819b584c87c4e4083ab27e262c7e41f11737
Karla Flores <U23235289@utp.edu.pe>
2026-04-19 15:33:21 -0500

    feat: configuracion local

--- ventas/src/main/java/cinerama/ventas/VentasApplication.java
+++ ventas/src/main/java/cinerama/ventas/VentasApplication.java
@@ -3,11 +3,10 @@ package cinerama.ventas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Spring Boot inicia, pero apaga la configuración de seguridad por defecto
+@SpringBootApplication (excludeName = {"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"})
 public class VentasApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(VentasApplication.class, args);
+        SpringApplication.run(VentasApplication.class, args);
 	}
-
 }
