41c8917ae46a1e23cb545f48be6c62fefa055f33
Karla Flores <U23235289@utp.edu.pe>
2026-04-19 23:53:03 -0500

    feat: creación de SecurityConfig

--- ventas/src/main/java/cinerama/ventas/VentasApplication.java
+++ ventas/src/main/java/cinerama/ventas/VentasApplication.java
@@ -2,24 +2,10 @@ package cinerama.ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class VentasApplication {
 	public static void main(String[] args) {
         SpringApplication.run(VentasApplication.class, args);
 	}
-
-    // Se pone la configuración de seguridad provisional aquí para las pruebas con postman
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Desactiva protección para poder hacer post
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
 }
