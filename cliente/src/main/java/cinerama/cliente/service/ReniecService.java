ec5c7a4c3d19b2bd46d1688685ee89bab9d63d01
SandroCy6 <diazsandro844@gmail.com>
2026-04-19 23:25:19 -0500

    update

--- cliente/src/main/java/cinerama/cliente/service/ReniecService.java
+++ cliente/src/main/java/cinerama/cliente/service/ReniecService.java
@@ -2,7 +2,6 @@ package cinerama.cliente.service;
 
 import cinerama.cliente.dto.reniec.ReniecData;
 import cinerama.cliente.dto.reniec.ReniecResponse;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
