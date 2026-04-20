ded6015a1c262b3edb1227a7f0a997073643cdac
Karla Flores <U23235289@utp.edu.pe>
2026-04-19 23:28:45 -0500

    feat: modificación de configuracion provisional para el uso de postman

--- ventas/src/main/java/cinerama/ventas/VentasApplication.java
+++ ventas/src/main/java/cinerama/ventas/VentasApplication.java
@@ -2,11 +2,24 @@ package cinerama.ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// Spring Boot inicia, pero apaga la configuración de seguridad por defecto
-@SpringBootApplication (excludeName = {"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"})
+@SpringBootApplication
 public class VentasApplication {
 	public static void main(String[] args) {
         SpringApplication.run(VentasApplication.class, args);
 	}
+
+    // Se pone la configuración de seguridad provisional aquí para las pruebas con postman
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desactiva protección para poder hacer post
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
 }
