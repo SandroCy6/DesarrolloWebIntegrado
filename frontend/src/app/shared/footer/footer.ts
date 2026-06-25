4c62c2a60487162ed08e95ce76167b045c907f64
SandroCy6 <diazsandro844@gmail.com>
2026-06-25 18:16:43 -0500

    feat: header y footer public con navegación y estilos Cinerama

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  year = new Date().getFullYear();
+}
